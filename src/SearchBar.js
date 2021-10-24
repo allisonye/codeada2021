2fc272bd9f2289ddb62773d1762d5044ae1ae075
aadyadoma <aadya.doma@gmail.com>
2021-10-24 13:20:14 -0500

    more dining halls

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -5,10 +5,11 @@ class SearchBar extends React.Component {
     render() {
         return (
                 <div class="topnav">
-                <a class="active" href="#home">Home</a>
-                <a href="#about">Breakfast</a>
-                <a href="#contact">Lunch</a>
-                <a href="#contact">Dinner</a>
+                <a class="active" href="#ISR">ISR</a>
+                <a href="#PAR">PAR</a>
+                <a href="#Ikenberry">Ikenberry</a>
+                <a href="#FAR">FAR</a>
+                <a href="#LAR">LAR</a>
                 <input type="text" placeholder="Search" />
                 <button type="submit"><i class="fa fa-search"></i></button>
             </div>
