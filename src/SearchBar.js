4e79c37c6444afc42ddfe22a66ff2c6b701c6c76
aadyadoma <aadya.doma@gmail.com>
2021-10-24 10:39:40 -0500

    search bar updated

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,8 +4,11 @@ import './index.css';
 class SearchBar extends React.Component {
     render() {
         return (
-            <div>
-                {/* html code*/}
+                <div class="topnav">
+                <a class="active" href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#contact">Contact</a>
+                <input type="text" placeholder="Search..">
             </div>
         );
     }
