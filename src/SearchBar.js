90549e1a9219039dab75a7c4ae92cc3dd0b68cfd
aadyadoma <aadya.doma@gmail.com>
2021-10-24 13:04:15 -0500

    changed names

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -5,15 +5,10 @@ class SearchBar extends React.Component {
     render() {
         return (
                 <div class="topnav">
-                <a class="active" href="#home">Home</a>
-<<<<<<< HEAD
-                <a href="#about">Breakfast</a>
-                <a href="#contact">Lunch</a>
-                <a href="#contact">Dinner</a>
-=======
-                <a href="#about">About</a>
-                <a href="#contact">Contact</a>
->>>>>>> 63f1f65ff7883bb1d7ee4c94986253b2d1dfcd8c
+                <a class="active" href="#home">ISR</a>
+                <a href="#about">PAR</a>
+                <a href="#contact">Ikenberry</a>
+                <a href="#contact">FAR</a>
                 <input type="text" placeholder="Search" />
                 <button type="submit"><i class="fa fa-search"></i></button>
             </div>
